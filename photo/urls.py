034c52b5c1313ea9c0abaa9dbfd0842896714335
Wecode <wecode@wecode>
2019-03-16 10:44:44 +0200

    fixing errors in migrations and updating the admin file

--- photo/urls.py
+++ photo/urls.py
@@ -9,5 +9,5 @@ urlpatterns=[
     url(r'^search/',views.search_category,name='search_category'),
 ]
 
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
