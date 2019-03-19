99d6466cf3dcd246f1e1fac2204f36079a835645
Wecode <wecode@wecode>
2019-03-19 13:16:26 +0200

    fisxing errors with migrations

--- photo/models.py
+++ photo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Category(models.Model):
     name = models.CharField(max_length = 20)
@@ -40,6 +41,7 @@ class Image(models.Model):
     image_description = models.TextField()
     image_category = models.ForeignKey(Category,on_delete = models.CASCADE)
     image_location = models.ForeignKey(Location,on_delete = models.CASCADE)
+    
 
 
     def save_image(self):
@@ -52,5 +54,3 @@ class Image(models.Model):
     def search_by_category(cls,search_term):
         images = cls.objects.filter(image_category__name__contains = search_term)
         return images
-
-    
